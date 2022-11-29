452ae5057cae96608eeb4b4e281109d277fb0329
trisongz <4735784+trisongz@users.noreply.github.com>
2022-11-29 15:41:56 -0600

    Update backend_bootstrap.sh

--- scripts/lotus/backend_bootstrap.sh
+++ scripts/lotus/backend_bootstrap.sh
@@ -3,22 +3,30 @@
 # This script is used to bootstrap the backend configuration for the lotus node.
 
 RUN_MODE=${RUN_MODE:-api}
-# api, celery, celery-beat
+# api, celery, celery-beat, event-consumer
 RUN_CMD=${RUN_CMD:-}
 CMD=""
 
 if [[ "$RUN_CMD" != "" ]]; then
     CMD="$RUN_CMD"
     echo " - Running Custom Command: ${CMD}"
+
 elif [[ "$RUN_MODE" == "api" ]]; then
     CMD="sh -c './scripts/start_backend.prod.sh'"
     echo " - Starting: API Server: ${CMD}"
+
+elif [[ "$RUN_MODE" == "event-consumer" ]]; then
+    CMD="sh -c './scripts/start_consumer.sh'"
+    echo " - Starting: Event Consumer: ${CMD}"
+
 elif [[ "$RUN_MODE" == "celery" ]]; then
     CMD="celery -A lotus worker -l info"
     echo " - Starting: Celery Worker: ${CMD}"
+
 elif [[ "$RUN_MODE" == "celery-beat" ]]; then
     CMD="celery -A lotus beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler"
     echo " - Starting: Celery Beat: ${CMD}"
+
 fi
 
 
